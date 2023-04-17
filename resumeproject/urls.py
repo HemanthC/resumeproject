64d959b6d14f72d12dba26b6c862642908d478a6
HEMANTHC <hemanthseervi21821@gmail.com>
2023-04-17 15:08:17 +0530

    deleting pychache

--- resumeproject/urls.py
+++ resumeproject/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+def hello(req):
+    return HttpResponse('hello')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello)
 ]
